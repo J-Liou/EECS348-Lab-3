8af70a9e7a1e21fdacd87257a0f9085a953ae6b8
j506l533 <jonathan.liou@ku.edu>
2026-09-17 13:04:22 -0500

    finish

--- MultiplicationTable/multiplication_table.php
+++ MultiplicationTable/multiplication_table.php
@@ -1,53 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Multiplication Table</title>
-
-    <style>
-        table {
-            border-collapse: collapse;
-        }
-
-        th, td {
-            padding: 8px 12px;
-            text-align: center;
-            border: 1px solid black;
-        }
-
-        th {
-            background-color: #eee;
-        }
-    </style>
-</head>
-
-<body>
-
 <?php
 
 $size = $_POST['size'];
 
-echo "<h2>Multiplication Table ($size x $size)</h2>";
+echo "<table border='1'>";
 
-echo "<table>";
-
-// Top-left corner
+// Top row
 echo "<tr>";
 echo "<th></th>";
 
-// Top row: 1, 2, 3, ... size
 for ($i = 1; $i <= $size; $i++) {
     echo "<th>$i</th>";
 }
 
 echo "</tr>";
 
-// Table rows
+// Remaining rows
 for ($i = 1; $i <= $size; $i++) {
 
     echo "<tr>";
 
-    // Left column
+    // First column
     echo "<th>$i</th>";
 
     // Multiplication values
@@ -61,6 +33,3 @@ for ($i = 1; $i <= $size; $i++) {
 echo "</table>";
 
 ?>
-
-</body>
-</html>
